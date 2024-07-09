2e675f8451ed2744552c066691c965a890e48734
mnajjaa <ibtihel.mnaja@esprit.tn>
2024-07-10 00:27:23 +0100

    initial commit

---
+++ src/main/java/com/projetSpringBoot_Ng/projetSg_Ng/ProjetSgNgApplication.java
@@ -0,0 +1,13 @@
+package com.projetSpringBoot_Ng.projetSg_Ng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSgNgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetSgNgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetSpringBoot_Ng/projetSg_Ng/ProjetSgNgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetSpringBoot_Ng.projetSg_Ng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSgNgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
