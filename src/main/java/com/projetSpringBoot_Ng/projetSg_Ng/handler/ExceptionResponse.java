d9ce137e564ef43eb69cff3a79c42a54b4c8a2e5
mnajjaa <ibtihel.mnaja@esprit.tn>
2024-07-17 21:09:14 +0100

    Exception handling

---
+++ src/main/java/com/projetSpringBoot_Ng/projetSg_Ng/handler/ExceptionResponse.java
@@ -0,0 +1,26 @@
+package com.projetSpringBoot_Ng.projetSg_Ng.handler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ExceptionResponse {
+
+    private Integer businessErrorCode;
+    private String businessErrorDescription;
+    private String error;
+    private Set<String> validationErrors; //this will hold the validation errors
+    private Map<String, String> errors; //this will hold the errors
+}
