1f98fcd557ac4944674c5a2051edf93173bee500
mnajjaa <ibtihel.mnaja@esprit.tn>
2024-08-20 21:47:52 +0100

    offer list without test

--- Platfrom-UI-01/src/app/modules/offre/pages/offre-list/offre-list.component.ts
+++ Platfrom-UI-01/src/app/modules/offre/pages/offre-list/offre-list.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {OffresService} from "../../../../services/services/offres.service";
+import {OffreResponse} from "../../../../services/models/offre-response";
 
 
 @Component({
@@ -7,9 +9,30 @@ import {Router} from "@angular/router";
   templateUrl: './offre-list.component.html',
   styleUrls: ['./offre-list.component.css']
 })
-export class OffreListComponent {
+export class OffreListComponent implements OnInit{
+  offerResponse: OffreResponse[] = [];
+   page= 0;
+   size= 5;
 constructor(
-
+  private offreService: OffresService,
+  private router: Router
 ) {
 }
+
+  ngOnInit(): void {
+  this.findAllOffres();
+  }
+
+
+  private findAllOffres() {
+  this.offreService.getAllOffres({
+    page: this.page,
+    size: this.size
+  }).subscribe({
+    next:(offres)=>{
+    this.offerResponse = offres;
+    }
+  });
+
+  }
 }
